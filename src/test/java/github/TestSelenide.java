6908fe296a2e2af2a1b97d788151468f7d48e615
Evgenii <sergeevjane@mail.ru>
2022-11-08 00:09:38 +0300

    added homework

---
+++ src/test/java/github/TestSelenide.java
@@ -0,0 +1,52 @@
+package github;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestSelenide {
+
+    @BeforeAll
+    static void setUp(){
+    Configuration.holdBrowserOpen = true;
+
+    }
+
+    @Test
+    @DisplayName("Проверка открытия раздела Enterprise на GitHub")
+    public void checkPageLabel(){
+        open("https://github.com/");
+        $(byText("Solutions")).hover();
+        $(byText("Enterprise")).click();
+        $("h1").shouldHave(text("Build like the best"));
+    }
+
+    @Test
+    @DisplayName("Проверка работы метода drag and drop")
+    public void dragAndDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    @DisplayName("Проверка работы метода drag and drop через actions")
+    public void dragAndDropWithActionsTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
